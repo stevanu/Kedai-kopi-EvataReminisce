a8f4092bf9ed13206f9234bfa69c37c536d01239
stevanu <ivanstevanu@gmail.com>
2025-01-22 23:31:23 +0700

    update alpine

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
-//  document.addEventListener('alpine:init', () => {
-//     Alpine.data('products', () => ({
-//         items: [
-//             { id: 1, name: 'Frappuccino', img: 'Frappuccino.jpg', price: 27000 },
-//             { id: 2, name: 'Americano', img: '2.jpg', price: 25000 },
-//             { id: 3, name: 'Espresso', img: '3.jpg', price: 19000 },
-//             { id: 4, name: 'Affogato', img: 'affogato.jpg', price: 30000 },
-//             { id: 5, name: 'Cortado', img: 'Cortado.jpg', price: 32000 },
-//             { id: 5, name: 'Moca', img: 'moca.jpg', price: 23000 },
-//             { id: 5, name: 'Cappuccino', img: 'Cappuccino.jpg', price: 26000 },
-//             { id: 5, name: 'Cold Brew', img: 'cold-brew.jpg', price: 31000 }, 
-//         ]
-//     }));
-//  });
  document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
