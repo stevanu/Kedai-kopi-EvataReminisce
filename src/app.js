43761aaa4c9c73908e8930e91f7fb842e1bebd56
stevanu <ivanstevanu@gmail.com>
2025-01-31 16:59:15 +0700

    Update Shopping cart

--- src/app.js
+++ src/app.js
@@ -10,4 +10,75 @@
             { id: 7, name: 'Mockup Black', img:'7.jpg', price: 86000 },
         ]
     }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            // cek apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // jika belum ada  / cart masih kosong 
+
+            if(!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+            } else {
+                // jika barang sudah ada, cek apakah barang beda atau sama dengan yang ada di cart
+                this.items = this.items.map((item) => {
+                // jika barang berbeda
+                if (item.id !== newItem.id) {
+                    return item;
+                } else {
+                    // jika barang sudah ada, tambah quantity dan totalnya 
+                    item.quantity++;
+                    item.total = item.price * item.quantity;
+                    this.quantity++;
+                    this.total += item.price;
+                    return item;
+                }
+                });
+            }
+
+        },
+        remove(id) {
+            // ambil item yang ingin diremove berdasarkan idnya
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jika item lebih dari 1
+            if (cartItem.quantity > 1) {
+                // telusuri satu satu 
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang diklik 
+                    if(item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+            //  jika barangnya sisa 1
+            this.items = this.items.filter((item) => item.id !== id);
+            this.quantity--;
+            this.total -= cartItem.price;
+            }
+        },
+    });
  });
+
+
+ // konversi ke Rupiah
+
+ const rupiah = (Number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(Number);
+ };
